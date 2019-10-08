c207df62f4a3416da932c214b834ff1b6ffae8e2
neal_jq <984870240@qq.com>
2019-10-08 18:57:35 +0800

    Add Role/User entity

---
+++ rental/src/main/java/com/ctbu/rental/service/UserService.java
@@ -0,0 +1,12 @@
+package com.ctbu.rental.service;
+
+import com.ctbu.rental.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService {
+
+    @Autowired
+    UserRepository userRepository;
+}
